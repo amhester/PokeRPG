a9cf86e3a1732d8dfc4ee435f9de4e8f912ab44a
amhester <amhester@anderson.edu>
2015-06-28 13:26:48 -0400

    Finished auth stuff and create user. We good now!

--- server/api.js
+++ server/api.js
@@ -3,16 +3,19 @@
  */
 var restify = require('restify');
 var db = require('mongoose');
-db.connect('');
+db.connect('mongodb://localhost/PokeRPG');
+var passHash = require('password-hash');
+var User = require('../server/models/UserModel');
 
 /* ----------------  Stuff to be refactored out at some point  ----------------- */
 //Some convienance methods
-function findFirstOrDefault(arr, compFun) {
-    arr.forEach(function (el, i) {
-        if(compFun(el)) {
-            return el;
+function authStoreHasToken(token) {
+    for(var i =  0; i < authTokenStore.length; i++) {
+        if(authTokenStore[i].token == token) {
+            return true;
         }
-    });
+    }
+    return false;
 }
 
 //Used to store an object with token and userId for all currently signed in users (e.g. { token: 345=Gd2!d, id: 45 })
@@ -20,9 +23,19 @@ var authTokenStore = [];
 
 //Custom authentication check for requests
 function authenticateRequest(req, res, next) {
-    if(req.header('Authorization') && findFirstOrDefault(authTokenStore, function (el) { return el.token == req.header('Authorization').split(' ')[1]; })) {
+    var authHeader = req.header('Authorization');
+    var authHeaderToken = authHeader && authHeader.split(' ').length > 1 ? authHeader.split(' ')[1] : '';
+    console.log('current auth header' + authHeader);
+    console.log('current auth token, if it exists: ' + authHeaderToken);
+    console.log('tokenStore has token? ' + authStoreHasToken(authHeaderToken));
+    if(authHeader && authStoreHasToken(authHeaderToken)) {
+        console.log('authorized request');
         next();
-    } else if (req.path == '/token') {
+    } else if (req.url == '/token') {
+        console.log('not authorized, seeking authentication');
+        next();
+    } else if (req.method == 'POST' && req.url == '/user') {
+        console.log('not authorized, creating a user.');
         next();
     } else {
         res.send(403, "Unauthorized Access.");
@@ -37,7 +50,10 @@ var server = restify.createServer({
 });
 
 //Global restify server event handling
-server.on('uncaughtException', function (request, response, route, error) {});
+server.on('uncaughtException', function (request, response, route, error) {
+    console.error(error);
+    res.send(500, 'Unknown Server Exception.');
+});
 
 //Generic handlers for any incoming request (careful, executed in order!)
 server.use(restify.acceptParser(server.acceptable));
@@ -45,17 +61,28 @@ server.use(authenticateRequest);
 server.use(restify.CORS());
 server.use(restify.dateParser());
 server.use(restify.queryParser());
+server.use(restify.bodyParser());
 
 //Actual api routes
 //Token route for authentication
 server.post('/token', function (req,res, err) {
-
+    console.log('Looking for a token...');
+    User.authenticate(req.params.email, req.params.password, function (results) {
+        if(results.ok) {
+            var token = new Buffer(Math.floor((Math.random() * 100000000) + 1000).toString() + results.user._id.toString()).toString('base64');
+            authTokenStore.push({ token: token, id: results.user._id });
+            res.send(200, {token: token});
+            console.log(authTokenStore);
+        } else {
+            res.send(400, "Bad Request");
+        }
+    });
 });
 
 //Account user routes
 var userPath = '/user';
 server.get(userPath, function (req, res, err) {
-
+    res.send(200, 'dummy path');
 });
 
 server.get(userPath + '/:id', function (req, res, err) {
@@ -67,7 +94,20 @@ server.put(userPath + '/:id', function (req, res, err) {
 });
 
 server.post(userPath, function (req, res, err) {
+    console.log('hit post user...');
 
+    var newUser = new User({
+        email: req.params.email,
+        password: req.params.password
+    });
+
+    newUser.save(function (err) {
+        if(err) {
+            console.error(err);
+            res.send(500, 'Error creating user.\\n' + err);
+    }
+        res.send(200, 'User successfully created.');
+    });
 });
 
 server.del(userPath + ':id', function (req, res, err) {
@@ -75,4 +115,6 @@ server.del(userPath + ':id', function (req, res, err) {
 });
 
 //Start server listening...
-server.listen(8090, '127.0.0.1');
+server.listen(8090, '127.0.0.1', function () {
+    console.log('api running, listening on 127.0.0.1:8090');
+});
--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -4,24 +4,41 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passHash = require('password-hash');
+var q = require('q');
 
 var userSchema = new Schema({
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, select: false },
+    password: { type: String, required: true },
     createdDate: Date,
     updatedDate: Date,
     LastOnline: Date
 });
 
 userSchema.pre('save', function (next) {
+    var currentDate = new Date();
+
+    this.updatedDate = currentDate;
+
     //TODO: function to hash password and set password to new hashed password
+    if(this.isNew) {
+        this.createdDate = currentDate;
+        this.password = passHash.generate(this.password);
+    }
 
     next();
 });
 
 userSchema.statics.authenticate = function (email, password, cb) {
-    var foundPass = this.findOne({ email: email }, 'password', function (err, pass) {
-
+    this.findOne({ email: email }, function (err, user) {
+        if(user) {
+            if(passHash.verify(password, user.password)) {
+                cb({ ok: true, message: "Valid user credentials.", user: user });
+            } else {
+                cb({ ok: false, message: "Invalid password!" });
+            }
+        } else {
+            cb({ ok: false, message: "No User with that email found." });
+        }
     });
 };
 
